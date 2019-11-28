ca4bc865f3fabe70815a98e0014047867c8577b3
Mohanad-Abdalradi <ar_mohanad@hotmail.com>
2019-11-28 14:24:13 +0100

    Pagination

---
+++ Section-4/app.js
@@ -0,0 +1,96 @@
+const btuRepos = document.getElementById("btuRepos");
+const btuIssues = document.getElementById("btuIssues");
+const btuCommits = document.getElementById("btuCommits");
+
+const divResult = document.getElementById("divResult");
+btuRepos.addEventListener("click", gitRepos);
+btuIssues.addEventListener("click", gitIssues);
+btuCommits.addEventListener("click", e=> gitCommits());
+
+async function gitRepos() {
+  clear();
+
+  const url =
+    "https://api.github.com/search/repositories?q=stars:100000..300000";
+  const response = await fetch(url);
+  const result = await response.json();
+
+  result.items.forEach(i => {
+    const anchor = document.createElement("a");
+    anchor.href = i.html_url;
+    anchor.textContent = i.full_name;
+    divResult.appendChild(anchor);
+    divResult.appendChild(document.createElement("br"));
+  });
+}
+
+async function gitIssues() {
+  clear();
+
+  const url =
+    "https://api.github.com/search/issues?q=author:raisedadead repo:freecodecamp/freecodecamp type:issue";
+  const response = await fetch(url);
+  const result = await response.json();
+
+  result.items.forEach(i => {
+    const anchor = document.createElement("a");
+    anchor.href = i.html_url;
+    anchor.textContent = i.title;
+    divResult.appendChild(anchor);
+    divResult.appendChild(document.createElement("br"));
+  });
+}
+
+async function gitCommits(
+  url = "https://api.github.com/search/commits?q=repo:freecodecamp/freecodecamp author-date:2019-03-01..2019-03-31"
+  ) {
+  clear();
+
+  const headers = {
+    Accept: "application/vnd.github.cloak-preview"
+  };
+  const response = await fetch(url, {
+    method: "GET",
+    headers: headers
+  });
+  //<https://api.github.com/search/commits?q=repo%3Afreecodecamp%2Ffreecodecamp+author-date%3A2019-03-01..2019-03-31&page=2>; rel="next", <https://api.github.com/search/commits?q=repo%3Afreecodecamp%2Ffreecodecamp+author-date%3A2019-03-01..2019-03-31&page=28>; rel="last"
+
+  const link = response.headers.get("link");
+  const links = link.split(",");
+  const urls = links.map(a => {
+    return {
+      url: a
+        .split(";")[0]
+        .replace(">", "")
+        .replace("<", ""),
+      title: a.split(";")[1]
+    };
+  });
+
+  const result = await response.json();
+
+  result.items.forEach(i => {
+    const img = document.createElement("img");
+    img.src = i.author.avatar_url;
+    img.style.width = "32px";
+    img.style.height = "32px";
+
+    const anchor = document.createElement("a");
+    anchor.href = i.html_url;
+    anchor.textContent = i.commit.message.substr(0, 120) + "...";
+    divResult.appendChild(img);
+    divResult.appendChild(anchor);
+    divResult.appendChild(document.createElement("br"));
+  });
+
+  urls.forEach(u=> {
+    const btu = document.createElement("button");
+    btu.textContent = u.title;
+    btu.addEventListener("click", e=> getCommits(u.url));
+    divResult.appendChild(btu);
+  });
+}
+
+function clear() {
+  while (divResult.firstChild) divResult.removeChild(divResult.firstChild);
+}
