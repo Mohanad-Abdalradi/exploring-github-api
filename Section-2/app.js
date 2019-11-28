8db7b4eb14b758c96e11583ac811d89bf653a19d
Mohanad-Abdalradi <ar_mohanad@hotmail.com>
2019-11-28 14:25:06 +0100

    Search issues

--- Section-2/app.js
+++ Section-2/app.js
@@ -1,46 +1,44 @@
+const btuRepos = document.getElementById("btuRepos");
+const btuIssues = document.getElementById("btuIssues");
 
-const btuRepos = document.getElementById('btuRepos')
-const btuIssues = document.getElementById('btuIssues')
-
-const divResult = document.getElementById("divResult")
-btuRepos.addEventListener('click', gitRepos)
-btuIssues.addEventListener('click', gitIssues)
+const divResult = document.getElementById("divResult");
+btuRepos.addEventListener("click", gitRepos);
+btuIssues.addEventListener("click", gitIssues);
 
 async function gitRepos() {
-    clear();
+  clear();
 
-    const url = "https://api.github.com/search/repositories?q=stars:100000..300000"
-    const response = await fetch(url)
-    const result = await response.json()
+  const url =
+    "https://api.github.com/search/repositories?q=stars:100000..300000";
+  const response = await fetch(url);
+  const result = await response.json();
 
-    result.items.forEach(i=>{
-
-        const anchor = document.createElement("a")
-        anchor.href = i.html_url;
-        anchor.textContent = i.full_name;
-        divResult.appendChild(anchor)
-        divResult.appendChild(document.createElement('br'))
-  })
+  result.items.forEach(i => {
+    const anchor = document.createElement("a");
+    anchor.href = i.html_url;
+    anchor.textContent = i.full_name;
+    divResult.appendChild(anchor);
+    divResult.appendChild(document.createElement("br"));
+  });
 }
 
 async function gitIssues() {
-    clear();
+  clear();
 
-    const url = "https://api.github.com/search/issues?q=author:raisedadead repo:freecodecamp/freecodecamp type:issue"
-    const response = await fetch(url)
-    const result = await response.json()
+  const url =
+    "https://api.github.com/search/issues?q=author:raisedadead repo:freecodecamp/freecodecamp type:issue";
+  const response = await fetch(url);
+  const result = await response.json();
 
-    result.items.forEach(i=>{
-
-        const anchor = document.createElement("a")
-        anchor.href = i.html_url;
-        anchor.textContent = i.title;
-        divResult.appendChild(anchor)
-        divResult.appendChild(document.createElement('br'))
-  })
+  result.items.forEach(i => {
+    const anchor = document.createElement("a");
+    anchor.href = i.html_url;
+    anchor.textContent = i.title;
+    divResult.appendChild(anchor);
+    divResult.appendChild(document.createElement("br"));
+  });
 }
 
-
 function clear() {
-    while(divResult.firstChild) divResult.removeChild(divResult.firstChild)
+  while (divResult.firstChild) divResult.removeChild(divResult.firstChild);
 }
