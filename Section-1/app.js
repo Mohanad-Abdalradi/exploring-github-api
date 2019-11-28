6ecaca45b02fc19e54a93edc134a9da9b88f56f0
Mohanad-Abdalradi <ar_mohanad@hotmail.com>
2019-11-28 18:40:25 +0100

    Search repositories

--- Section-1/app.js
+++ Section-1/app.js
@@ -1,25 +1,24 @@
+const btuRepos = document.getElementById("btuRepos");
+const divResult = document.getElementById("divResult");
+btuRepos.addEventListener("click", getRepos);
 
-const btuRepos = document.getElementById('btuRepos')
-const divResult = document.getElementById("divResult")
-btuRepos.addEventListener('click', gitRepos)
+async function getRepos() {
+  clear();
 
-async function gitRepos() {
-    clear();
+  const url =
+    "https://api.github.com/search/repositories?q=stars:100000..300000";
+  const response = await fetch(url);
+  const result = await response.json();
 
-    const url = "https://api.github.com/search/repositories?q=stars:100000..300000"
-    const response = await fetch(url)
-    const result = await response.json()
-
-    result.items.forEach(i=>{
-
-        const anchor = document.createElement("a")
-        anchor.href = i.html_url;
-        anchor.textContent = i.full_name;
-        divResult.appendChild(anchor)
-        divResult.appendChild(document.createElement('br'))
+  result.items.forEach(i => {
+    const anchor = document.createElement("a");
+    anchor.href = i.html_url;
+    anchor.textContent = i.full_name;
+    divResult.appendChild(anchor);
+    divResult.appendChild(document.createElement("br"));
   });
 }
 
 function clear() {
-    while(divResult.firstChild) divResult.removeChild(divResult.firstChild)
+  while (divResult.firstChild) divResult.removeChild(divResult.firstChild);
 }
